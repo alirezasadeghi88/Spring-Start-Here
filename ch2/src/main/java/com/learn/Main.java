cf8ec5562c0aa1b609b5b2fc6c8d4a586f4f4ecf
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 04:59:34 -0700

    Add class main

--- ch2/src/main/java/com/learn/Main.java
+++ ch2/src/main/java/com/learn/Main.java
@@ -1,9 +1,14 @@
 package com.learn;
 
 import com.learn.model.Parrot;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.annotation.AnnotationTypeMismatchException;
 
 public class Main {
     public static void main(String[] args) {
+        var context =
+                new AnnotationConfigApplicationContext();
         Parrot parrot = new Parrot();
     }
 }
