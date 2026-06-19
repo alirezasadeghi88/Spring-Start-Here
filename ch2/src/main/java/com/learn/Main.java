05c3c106db6df7e11d5b98f61693b947e2c19508
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 05:08:38 -0700

    Add packge config

--- ch2/src/main/java/com/learn/Main.java
+++ ch2/src/main/java/com/learn/Main.java
@@ -12,7 +12,12 @@ public class Main {
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         Parrot parrot = context.getBean(Parrot.class);
-
         System.out.println(parrot.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println(s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
     }
 }
