b0d91985f481a943b68789eace1b9b5d785eba53
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 05:39:27 -0700

    spring start here packge config class Projectconfig

--- ch3/src/main/java/com/learn/config/ProjectConfig.java
+++ ch3/src/main/java/com/learn/config/ProjectConfig.java
@@ -3,23 +3,11 @@ package com.learn.config;
 import com.learn.model.Parrot;
 import com.learn.model.Person;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.learn.model")
 public class ProjectConfig {
 
-    @Bean
-    public Parrot parrot() {
-        Parrot parrot = new Parrot();
-        parrot.setName("KOKO");
-        return parrot;
-    }
-
-    @Bean
-    public Person person(Parrot parrot) {
-        Person person = new Person();
-        person.setName("ELLA");
-        person.setParrot(parrot());
-        return person;
-    }
 }
