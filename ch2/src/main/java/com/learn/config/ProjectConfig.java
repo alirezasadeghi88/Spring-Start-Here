54d3eb66b5a41dc8c6b16af5aecfe6a0b91f7f40
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 05:02:46 -0700

    Add packge config

--- ch2/src/main/java/com/learn/config/ProjectConfig.java
+++ ch2/src/main/java/com/learn/config/ProjectConfig.java
@@ -1,8 +1,16 @@
 package com.learn.config;
 
+import com.learn.model.Parrot;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectConfig {
 
+    @Bean
+    Parrot parrot() {
+        var p = new Parrot();
+        p.setName("koko");
+        return p;
+    }
 }
