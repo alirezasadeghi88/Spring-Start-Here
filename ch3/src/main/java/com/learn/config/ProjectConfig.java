9249794034da58f43ee9e0ebcf85ac8552b98e7f
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 05:26:29 -0700

    spring start here packge config class projectconfig

---
+++ ch3/src/main/java/com/learn/config/ProjectConfig.java
@@ -0,0 +1,24 @@
+package com.learn.config;
+
+import com.learn.model.Parrot;
+import com.learn.model.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot() {
+        Parrot parrot = new Parrot();
+        parrot.setName("KOKO");
+        return parrot;
+    }
+
+    @Bean
+    public Person person() {
+        Person person = new Person();
+        person.setName("ELLA");
+        return person;
+    }
+}
