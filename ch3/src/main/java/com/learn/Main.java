c0ca0225f24b8572c68f2ef5ebd74d0a04b4a785
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 05:31:38 -0700

    spring start here packge com.learn class main

--- ch3/src/main/java/com/learn/Main.java
+++ ch3/src/main/java/com/learn/Main.java
@@ -1,4 +1,20 @@
 package com.learn;
 
+import com.learn.config.ProjectConfig;
+import com.learn.model.Parrot;
+import com.learn.model.Person;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        var context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Person person = context.getBean(Person.class);
+        Parrot parrot = context.getBean(Parrot.class);
+
+        System.out.println("Person's name: " + person.getName());
+        System.out.println("Parrot's name: " + parrot.getName());
+        System.out.println("Person's parrot: " + person.getParrot());
+    }
 }
