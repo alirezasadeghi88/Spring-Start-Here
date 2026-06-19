ed5ad0f24358b54753d36441a285da170d98120f
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 05:41:04 -0700

    spring start here packge com.learn class main

--- ch3/src/main/java/com/learn/Main.java
+++ ch3/src/main/java/com/learn/Main.java
@@ -6,15 +6,14 @@ import com.learn.model.Person;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
+
     public static void main(String[] args) {
-        var context =
-                new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var context = new AnnotationConfigApplicationContext
+                (ProjectConfig.class);
 
-        Person person = context.getBean(Person.class);
-        Parrot parrot = context.getBean(Parrot.class);
+        Person p = context.getBean(Person.class);
 
-        System.out.println("Person's name: " + person.getName());
-        System.out.println("Parrot's name: " + parrot.getName());
-        System.out.println("Person's parrot: " + person.getParrot());
+        System.out.println("Person's name: " + p.getName());
+        System.out.println("Person's parrot: " + p.getParrot());
     }
 }
