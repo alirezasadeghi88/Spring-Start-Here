56e3484fc1cc88bebf460d15c1736fc77fbf5050
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-19 21:43:53 -0700

    spring start here packge model class parrot

--- ch3/src/main/java/com/learn/model/Parrot.java
+++ ch3/src/main/java/com/learn/model/Parrot.java
@@ -1,12 +1,18 @@
 package com.learn.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Parrot {
     private String name = "KOKO";
 
+    private final Person person;
 
+    @Autowired
+    public Parrot(Person person) {
+        this.person = person;
+    }
 
     public String getName() {
         return name;
