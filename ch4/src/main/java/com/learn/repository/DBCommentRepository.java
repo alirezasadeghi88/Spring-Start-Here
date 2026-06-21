20fdd63b6add1b41e20c66d79774e20d9e86c023
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:41:35 -0700

    spring start here ch4 packge repository class DBCommentRepository

--- ch4/src/main/java/com/learn/repository/DBCommentRepository.java
+++ ch4/src/main/java/com/learn/repository/DBCommentRepository.java
@@ -1,7 +1,9 @@
 package com.learn.repository;
 
 import com.learn.model.Comment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DBCommentRepository implements CommentRepository{
     @Override
     public void storeComment(Comment comment) {
