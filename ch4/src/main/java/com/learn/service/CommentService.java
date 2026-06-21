dab1b474c4ae7ac7ff6a37d84fb8cc893762243e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:54:58 -0700

    spring start here ch4 packge service class CommentService

--- ch4/src/main/java/com/learn/service/CommentService.java
+++ ch4/src/main/java/com/learn/service/CommentService.java
@@ -3,6 +3,7 @@ package com.learn.service;
 import com.learn.model.Comment;
 import com.learn.proxy.CommentNotificationProxy;
 import com.learn.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,8 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository,  @Qualifier("PUSH")
+        CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
