da071f3fbd20d2b8982b1a105501c22128a54bce
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:33:20 -0700

    spring start here ch4 packge service class CommentService

---
+++ ch4/src/main/java/com/learn/service/CommentService.java
@@ -0,0 +1,20 @@
+package com.learn.service;
+
+import com.learn.model.Comment;
+import com.learn.proxy.CommentNotificationProxy;
+import com.learn.repository.CommentRepository;
+
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment) {
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
