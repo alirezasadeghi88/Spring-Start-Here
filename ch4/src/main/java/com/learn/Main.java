9e8f0b07af4a65303e99ab0e4e55f50cdc3a2544
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:49:39 -0700

    spring start here ch4 packge com.learn class Main

--- ch4/src/main/java/com/learn/Main.java
+++ ch4/src/main/java/com/learn/Main.java
@@ -1,19 +1,21 @@
 package com.learn;
 
+import com.learn.config.ProjectConfiguration;
 import com.learn.model.Comment;
 import com.learn.proxy.EmailCommentNotificationProxy;
 import com.learn.repository.DBCommentRepository;
 import com.learn.service.CommentService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
+
         var comment = new Comment();
         comment.setAuthor("alireza");
         comment.setText("Hello");
 
+        var commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
     }
 }
