2217864c04bf8299b35df6be42f1637541e1cd1d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:36:36 -0700

    spring start here ch4 packge com.learn class Main

--- ch4/src/main/java/com/learn/Main.java
+++ ch4/src/main/java/com/learn/Main.java
@@ -1,4 +1,19 @@
 package com.learn;
 
+import com.learn.model.Comment;
+import com.learn.proxy.EmailCommentNotificationProxy;
+import com.learn.repository.DBCommentRepository;
+import com.learn.service.CommentService;
+
 public class Main {
+    public static void main(String[] args) {
+        var commentRepository = new DBCommentRepository();
+        var commentNotificationProxy = new EmailCommentNotificationProxy();
+        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+        var comment = new Comment();
+        comment.setAuthor("alireza");
+        comment.setText("Hello");
+
+        commentService.publishComment(comment);
+    }
 }
