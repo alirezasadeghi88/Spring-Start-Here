f1fa269b5f4fbb1be6121fe084ef0fce61677d7e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:27:02 -0700

    spring start here ch4 packge repository class DBCommentRepository

---
+++ ch4/src/main/java/com/learn/repository/DBCommentRepository.java
@@ -0,0 +1,10 @@
+package com.learn.repository;
+
+import com.learn.model.Comment;
+
+public class DBCommentRepository implements CommentRepository{
+    @Override
+    public void storeComment(Comment comment) {
+        System.out.println("Storing comment: " + comment.getText());
+    }
+}
