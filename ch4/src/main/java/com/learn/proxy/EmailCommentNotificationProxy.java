b12adefb4cc03b4a75c1563dc20905f35262cda9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-21 08:42:26 -0700

    spring start here ch4 packge proxy class EmailCommentNotificationProxy

--- ch4/src/main/java/com/learn/proxy/EmailCommentNotificationProxy.java
+++ ch4/src/main/java/com/learn/proxy/EmailCommentNotificationProxy.java
@@ -1,7 +1,9 @@
 package com.learn.proxy;
 
 import com.learn.model.Comment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
