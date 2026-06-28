c5858e572a2ad6b9a88ef26612d99cd0a6fc7c0b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-27 22:11:12 -0700

    add class CommentService

--- ch5/src/main/java/com/learn/service/CommentService.java
+++ ch5/src/main/java/com/learn/service/CommentService.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class CommentService {
 
-    @Autowired
+
     private CommentRepository commentRepository;
+
+    public CommentRepository getCommentRepository() {
+        return commentRepository;
+    }
 
 }
