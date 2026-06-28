d11aa619fb808ab70d53b3efd35b36aed64e8516
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-27 22:14:49 -0700

    add class

--- ch5/src/main/java/com/learn/Main.java
+++ ch5/src/main/java/com/learn/Main.java
@@ -9,8 +9,8 @@ public class Main {
         var c =
                 new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        var cs1 = c.getBean("commentService", CommentService.class);
-        var cs2 = c.getBean("commentService", CommentService.class);
+        var cs1 = c.getBean( CommentService.class);
+        var cs2 = c.getBean( User.class);
 
         boolean b1 = cs1 == cs2;
         System.out.println(b1);
--- ch5/src/main/java/com/learn/service/CommentService.java
+++ ch5/src/main/java/com/learn/service/CommentService.java
@@ -1,17 +1,4 @@
 package com.learn.service;
 
-import com.learn.repository.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class CommentService {
-
-
-    private CommentRepository commentRepository;
-
-    public CommentRepository getCommentRepository() {
-        return commentRepository;
-    }
-
 }
