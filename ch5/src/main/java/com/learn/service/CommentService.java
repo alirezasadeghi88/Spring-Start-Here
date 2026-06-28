11cbf8ab7751350eb362fe690e2d7a7c9741ca72
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-27 22:09:27 -0700

    add class CommentService

--- ch5/src/main/java/com/learn/service/CommentService.java
+++ ch5/src/main/java/com/learn/service/CommentService.java
@@ -1,9 +1,13 @@
 package com.learn.service;
 
+import com.learn.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
 
+    @Autowired
+    private CommentRepository commentRepository;
 
 }
