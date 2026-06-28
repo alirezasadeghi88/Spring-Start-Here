95823df321dfae70789a0a9dec1f2cb4b331d844
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-27 22:12:42 -0700

    add class

--- ch5/src/main/java/com/learn/config/ProjectConfig.java
+++ ch5/src/main/java/com/learn/config/ProjectConfig.java
@@ -2,13 +2,12 @@ package com.learn.config;
 
 import com.learn.service.CommentService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.learn.service", resourcePattern = "com.learn.repository")
 public class ProjectConfig {
 
-    @Bean
-    public CommentService commentService() {
-        return new CommentService();
-    }
+
 }
