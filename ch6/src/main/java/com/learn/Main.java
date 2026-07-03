1c258f7d41f74e289a5fb9f81fbdb760229bb02d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-02 21:44:10 -0700

    Add class main

--- ch6/src/main/java/com/learn/Main.java
+++ ch6/src/main/java/com/learn/Main.java
@@ -1,4 +1,18 @@
 package com.learn;
 
+import com.learn.config.ProjectConfig;
+import com.learn.model.Comment;
+import com.learn.service.CommentService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = c.getBean(CommentService.class);
+        Comment comment = new Comment();
+        comment.setText("hello");
+        comment.setAuthor("alireza");
+        service.publishComment(comment);
+    }
 }
