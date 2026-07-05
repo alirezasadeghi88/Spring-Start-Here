6d3f8486d6befbbf136aca8975f9d30b24730e00
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-04 21:47:12 -0700

    Add class LoggingAspect

--- ch6/src/main/java/com/learn/model/LoggingAspect.java
+++ ch6/src/main/java/com/learn/model/LoggingAspect.java
@@ -4,8 +4,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import java.util.logging.Logger;
+
 @Aspect
 public class LoggingAspect {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
     @Around("execution(* com.learn.service.*.*(..))")
     public void log(ProceedingJoinPoint joinPoint) throws Throwable {
         joinPoint.proceed();
