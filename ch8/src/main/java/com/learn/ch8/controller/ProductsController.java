d48f52c48a164a9d6ab6278994e7173cc7634d2a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 08:22:19 +0330

    Add class ProductService

--- ch8/src/main/java/com/learn/ch8/controller/ProductsController.java
+++ ch8/src/main/java/com/learn/ch8/controller/ProductsController.java
@@ -1,4 +1,26 @@
 package com.learn.ch8.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.learn.ch8.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ProductsController {
+
+    private final ProductService productService;
+
+    public ProductsController(
+            ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String viewProducts(Model model) {
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
 }
