5e89221b11e08a259d028cd8399b9cb707b680e5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 08:11:49 +0330

    Add class PaymentService

--- ch_10/src/main/java/com/learn/ch_10/service/PaymentService.java
+++ ch_10/src/main/java/com/learn/ch_10/service/PaymentService.java
@@ -1,7 +1,11 @@
 package com.learn.ch_10.service;
 
+import com.learn.ch_10.exception.NotEnoughMoneyException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+    public PaymentDetails processPayment() {
+        throw new NotEnoughMoneyException();
+    }
 }
