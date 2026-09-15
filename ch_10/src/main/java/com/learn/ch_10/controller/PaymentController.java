c5d0d33cab0038487355dbdcc98736f368e2ce0a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 08:24:21 +0330

    Add class PaymentController

--- ch_10/src/main/java/com/learn/ch_10/controller/PaymentController.java
+++ ch_10/src/main/java/com/learn/ch_10/controller/PaymentController.java
@@ -11,17 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
-    private final PaymentService paymentService;
 
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
-
-    @PostMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
-        return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(paymentDetails);
-    }
 }
