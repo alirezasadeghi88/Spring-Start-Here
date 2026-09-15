0a63d921ec43596f041e155b0c981ecbd1be6ae3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 08:20:12 +0330

    Add class PaymentController

--- ch_10/src/main/java/com/learn/ch_10/controller/PaymentController.java
+++ ch_10/src/main/java/com/learn/ch_10/controller/PaymentController.java
@@ -1,7 +1,13 @@
 package com.learn.ch_10.controller;
 
+import com.learn.ch_10.service.PaymentService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 }
