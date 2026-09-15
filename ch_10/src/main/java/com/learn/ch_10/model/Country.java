05f7707d4509acc3eaf32501f9e340948a230053
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 08:04:20 +0330

    Add class Country

--- ch_10/src/main/java/com/learn/ch_10/model/Country.java
+++ ch_10/src/main/java/com/learn/ch_10/model/Country.java
@@ -4,4 +4,17 @@ public class Country {
 
     private String name;
     private int population;
+
+    public static Country of(
+            String name,
+            int population) {
+        Country country = new Country();
+        country.setName(name);
+        country.setPopulation(population);
+        return country;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
