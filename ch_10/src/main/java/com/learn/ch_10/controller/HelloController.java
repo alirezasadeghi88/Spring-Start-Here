2cf774914735237db0a3b31ddc89f8d48959070c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 08:01:04 +0330

    Add class HelloController

--- ch_10/src/main/java/com/learn/ch_10/controller/HelloController.java
+++ ch_10/src/main/java/com/learn/ch_10/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.learn.ch_10.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello!";
+    }
 }
