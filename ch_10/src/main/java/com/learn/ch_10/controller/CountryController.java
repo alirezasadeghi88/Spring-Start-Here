953f60ea4f646776bbafcd46a639102fa9ce62b6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 08:06:19 +0330

    Add class CountryController

--- ch_10/src/main/java/com/learn/ch_10/controller/CountryController.java
+++ ch_10/src/main/java/com/learn/ch_10/controller/CountryController.java
@@ -1,7 +1,14 @@
 package com.learn.ch_10.controller;
 
+import com.learn.ch_10.model.Country;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CountryController {
+    @GetMapping("/france")
+    public Country france() {
+        Country c = Country.of("France", 67);
+        return c;
+    }
 }
