aa74b3f9cd5afe85b77e320683712feaab394c62
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 10:13:43 +0330

    Add class PurchaseController

--- ch12/src/main/java/com/learn/ch12/controller/PurchaseController.java
+++ ch12/src/main/java/com/learn/ch12/controller/PurchaseController.java
@@ -1,6 +1,9 @@
 package com.learn.ch12.controller;
 
+import com.learn.ch12.model.Purchase;
 import com.learn.ch12.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/purchase")
 public class PurchaseController {
     private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
 }
