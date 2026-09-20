54ee6e9ba7094666160f07e155ef04de2d73f5c4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 10:06:45 +0330

    Add class Purchase

--- ch12/src/main/java/com/learn/ch12/Purchase.java
+++ ch12/src/main/java/com/learn/ch12/Purchase.java
@@ -6,4 +6,16 @@ public class Purchase {
     private int id;
     private String product;
     private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
 }
