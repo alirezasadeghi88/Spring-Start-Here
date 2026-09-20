c000d4bdb9e870e08af3069ff629ca4e82d63fb4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 10:10:07 +0330

    Add class PurchaseRepository

--- ch12/src/main/java/com/learn/ch12/repository/PurchaseRepository.java
+++ ch12/src/main/java/com/learn/ch12/repository/PurchaseRepository.java
@@ -1,9 +1,23 @@
 package com.learn.ch12.repository;
 
+import com.learn.ch12.model.Purchase;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class PurchaseRepository {
     private final JdbcTemplate jdbc;
+
+    public PurchaseRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    public void storePurchase(Purchase purchase) {
+        String sql =
+                "INSERT INTO purchase VALUES (NULL, ?, ?)";
+
+        jdbc.update(sql,
+                purchase.getProduct(),
+                purchase.getPrice());
+    }
 }
