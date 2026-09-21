4533f5a910405c624c3b657d4770c2b6ae1358d5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 14:46:35 +0330

    Add class TransferRequest

--- ch13/src/main/java/com/learn/ch13/model/TransferRequest.java
+++ ch13/src/main/java/com/learn/ch13/model/TransferRequest.java
@@ -1,4 +1,9 @@
 package com.learn.ch13.model;
 
+import java.math.BigDecimal;
+
 public class TransferRequest {
+    private long senderAccountId;
+    private long receiverAccountId;
+    private BigDecimal amount;
 }
