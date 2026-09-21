c88fd44b9f1bbf3d1e032df3d3d1cca1037e366a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 14:44:11 +0330

    Add class AccountController

--- ch13/src/main/java/com/learn/ch13/controller/AccountController.java
+++ ch13/src/main/java/com/learn/ch13/controller/AccountController.java
@@ -1,7 +1,9 @@
 package com.learn.ch13.controller;
 
+import com.learn.ch13.service.TransferService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountController {
+    private final TransferService transferService;
 }
