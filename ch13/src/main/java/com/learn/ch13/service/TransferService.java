821033935a6773351a764c94000a3c7b836032ea
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 14:41:52 +0330

    Add class TransferService

--- ch13/src/main/java/com/learn/ch13/service/TransferService.java
+++ ch13/src/main/java/com/learn/ch13/service/TransferService.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class TransferService {
     private final AccountRepository accountRepository;
+
+    public TransferService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
 }
