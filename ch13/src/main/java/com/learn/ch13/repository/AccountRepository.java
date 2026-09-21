5d7596be8454d496070994a53257662346917fd0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 14:36:10 +0330

    Add class AccountRepository

--- ch13/src/main/java/com/learn/ch13/repository/AccountRepository.java
+++ ch13/src/main/java/com/learn/ch13/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.learn.ch13.repository;
 
+import com.learn.ch13.model.Account;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,10 @@ public class AccountRepository {
 
     public AccountRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
+    }
+
+    public Account findAccountById(long id) {
+        String sql = "SELECT * FROM account WHERE id = ?";
+        return jdbc.queryForObject(sql, new AccountRowMapper(), id);
     }
 }
