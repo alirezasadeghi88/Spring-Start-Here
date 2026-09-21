f14984a0bd5ea4e45156d52f738739902d1854ea
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 14:42:37 +0330

    Add class TransferService

--- ch13/src/main/java/com/learn/ch13/service/TransferService.java
+++ ch13/src/main/java/com/learn/ch13/service/TransferService.java
@@ -1,7 +1,11 @@
 package com.learn.ch13.service;
 
+import com.learn.ch13.model.Account;
 import com.learn.ch13.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 public class TransferService {
@@ -9,5 +13,23 @@ public class TransferService {
 
     public TransferService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+    @Transactional
+    public void transferMoney(long idSender,
+                              long idReceiver,
+                              BigDecimal amount) {
+        Account sender =
+        accountRepository.findAccountById(idSender);
+        Account receiver =
+        accountRepository.findAccountById(idReceiver);
+        BigDecimal senderNewAmount =
+        sender.getAmount().subtract(amount);
+        BigDecimal receiverNewAmount =
+        receiver.getAmount().add(amount);
+        accountRepository
+     .changeAmount(idSender, senderNewAmount);
+        accountRepository
+     .changeAmount(idReceiver, receiverNewAmount);
     }
 }
