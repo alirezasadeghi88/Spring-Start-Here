fbfd23429453c3d393173d4f8970c08e5b8512f6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-26 17:25:37 +0330

    Add class AccountController

--- ch_14/src/main/java/com/learn/ch_14/controller/AccountController.java
+++ ch_14/src/main/java/com/learn/ch_14/controller/AccountController.java
@@ -1,4 +1,27 @@
 package com.learn.ch_14.controller;
 
+import com.learn.ch_14.service.TransferService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
+
+    private final TransferService transferService;
+
+    public AccountController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public void transferMoney(
+                                              @RequestBody TransferRequest request
+    ) {
+        transferService.transferMoney(
+                request.getSenderAccountId(),
+                request.getReceiverAccountId(),
+                request.getAmount());
+    }
+
 }
