069b2c34cca603442d67daaac44a1ff250a849b7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-26 17:17:12 +0330

    Add class TransferService

--- ch_14/src/main/java/com/learn/ch_14/service/TransferService.java
+++ ch_14/src/main/java/com/learn/ch_14/service/TransferService.java
@@ -1,7 +1,12 @@
 package com.learn.ch_14.service;
 
+import com.learn.ch_14.model.Account;
 import com.learn.ch_14.repository.AccountRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
 
 @Service
 public class TransferService {
@@ -10,5 +15,32 @@ public class TransferService {
 
     public TransferService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
+    }
+
+    @Transactional
+    public void transferMoney(
+            long idSender,
+            long idReceiver,
+            BigDecimal amount) throws AccountNotFoundException {
+
+        Account sender =
+        accountRepository.findById(idSender)
+        .orElseThrow(() -> new AccountNotFoundException());
+
+        Account receiver =
+        accountRepository.findById(idReceiver)
+                .orElseThrow(() -> new AccountNotFoundException());
+
+        BigDecimal senderNewAmount =
+        sender.getAmount().subtract(amount);
+
+        BigDecimal receiverNewAmount =
+        receiver.getAmount().add(amount);
+
+        accountRepository
+      .changeAmount(idSender, senderNewAmount);
+
+        accountRepository
+      .changeAmount(idReceiver, receiverNewAmount);
     }
 }
